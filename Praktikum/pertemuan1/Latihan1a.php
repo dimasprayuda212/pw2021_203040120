a63b0c71592ba5f9dc6549b4665c2a1ff905d8cd
dimasprayuda212 <dimassprayuda212@gmail.com>
2021-03-02 11:26:46 +0700

    menyelesaikan tugas praktikum 1

---
+++ Praktikum/pertemuan1/Latihan1a.php
@@ -0,0 +1,14 @@
+<?php 
+    // Dimas Prayuda
+    // 203040120
+    // Jum'at 13.00-14.00
+    // https://github.com/dimasprayuda212/pw2021_203040120
+    // Tugas Praktikum PW
+?>
+
+<?php for ($x = 1; $x <= 3; $x++) {
+    for ($z = 1; $z <= 3; $z++){
+        echo "Ini perulangan ke ($x,$z)<br>";
+    }
+}
+?>
---
+++ Praktikum/pertemuan1/Latihan1b.php
@@ -0,0 +1,36 @@
+<?php 
+    // Dimas Prayuda
+    // 203040120
+    // Jum'at 13.00-14.00
+    // https://github.com/dimasprayuda212/pw2021_203040120
+    // Tugas Praktikum PW
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1b DimasPrayuda</title>
+</head>
+<body>
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th></th>
+            <?php for ($i = 1; $i <= 5; $i++) : ?>
+                <th>Kolom <?= $i; ?></th>
+            <?php endfor; ?>
+        </tr>
+        
+        <!-- Tambahkan Baris Kode PHP Untuk Menampilkan Body Table -->
+        <?php for ($i = 1; $i <= 5; $i++) : ?>
+        <tr>
+            <th>Baris <?= $i; ?></th>
+            <?php for ($z = 1; $z <= 5; $z++) : ?>
+                <td>Baris <?= $i ?>, Kolom <?= $z; ?></td>
+            <?php endfor; ?>
+        </tr>
+        <?php endfor; ?>
+    </table>
+</body>
+</html>
